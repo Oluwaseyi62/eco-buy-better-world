7be8460fd2a9d55a618660aa701484baf85fe88c
Victor Essien <viktoressien1@gmail.com>
2025-04-22 17:23:22 +0100

    Made changes for deploying

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    const connnection = await mongoose.connect(process.env.MONGO_URI as string, {
-      tls: true, // Ensure TLS is enabled
-    });
+    const connnection = await mongoose.connect(
+      process.env.MONGO_URI as string,
+      {
+        tls: true, // Ensure TLS is enabled
+      }
+    );
     console.log(`Database Connected Successfully`);
   } catch (error) {
     console.log("DB Error:" + error);
